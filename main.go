5cd5ecd8132ab03bd10fa6ff7819ac2935ae4fc8
rikuyaosawa <rikuyaosawa@gmail.com>
2024-11-24 16:31:27 +0900

    getting started

---
+++ main.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+	server := gin.Default()
+	server.LoadHTMLGlob("templates/*")
+
+	server.GET("/", getLayoutTemplate)
+
+	server.Run(":1010")
+}
+
+func getLayoutTemplate(c *gin.Context) {
+	c.HTML(http.StatusOK, "layout.tmpl", gin.H{
+		"title": "Web Portfolio in HTMX/Go",
+	})
+}
